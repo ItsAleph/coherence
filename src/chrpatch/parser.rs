1742a7ce3f5192019e3205520b7e33feef5ceb7b
ItsAleph <igorekkrupskij@gmail.com>
2023-04-28 22:02:58 +0300

    docs: add contributin section

--- src/chrpatch/parser.rs
+++ src/chrpatch/parser.rs
@@ -3,3 +3,12 @@ use pest_derive::Parser;
 #[derive(Parser)]
 #[grammar = "./chrpatch/chrpatch.pest"]
 pub struct CHRPatchParser;
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use crate::chrpatch::parser::CHRPatchParser;
+
+    #[test]
+    fn t() {}   
+}
