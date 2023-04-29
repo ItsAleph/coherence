8e1ada0d242c005da4c477677afbf0a7a19495c7
ItsAleph <igorekkrupskij@gmail.com>
2023-04-29 08:18:46 +0000

    stuff(stuff): stuff stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod chrpatch;
 mod cli;
 
+use pest::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    for pair in crate::chrpatch::parser::CHRPatchParser::parse(
+        crate::chrpatch::parser::Rule::file,
+        &std::fs::read_to_string("test.chrpatch").unwrap(),
+    )
+    .unwrap()
+    {
+        println!("{:?}", pair);
+    }
 }
